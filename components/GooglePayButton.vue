32c101fd814025a3c76b44777b9a68bb859031f3
Dmitry Kozachek <dmitry.kozachek@gmail.com>
2021-09-08 17:11:42 -0400

    fix(button): Fix button component issues

--- components/GooglePayButton.vue
+++ components/GooglePayButton.vue
@@ -3,22 +3,22 @@
 </template>
 
 <script>
-import { ref, computed, onMounted } from '@vue/composition-api';
-import { useVSFContext } from '@vue-storefront/core';
+import { ref, computed, onMounted } from "@vue/composition-api";
+import { useVSFContext } from "@vue-storefront/core";
 
 const baseRequest = {
   apiVersion: 2,
-  apiVersionMinor: 0
+  apiVersionMinor: 0,
 };
 
-const ENV_PROD = 'production';
+const ENV_PROD = "production";
 
 let googlePayScriptLoaded = false;
 let paymentsClient = null;
-const lineItemsToDisplay = ['subtotal', 'tax'];
+const lineItemsToDisplay = ["subtotal", "tax"];
 
 export default {
-  name: 'GooglePayButton',
+  name: "GooglePayButton",
   props: {
     totals: {
       type: Object,
@@ -27,42 +27,43 @@ export default {
     type: {
       type: String,
       required: false,
-      default: 'long',
+      default: "long",
       validator: function (value) {
-        return ['long', 'short'].indexOf(value) !== -1
-      }
+        return ["long", "short"].indexOf(value) !== -1;
+      },
     },
     color: {
       type: String,
       required: false,
-      default: 'default',
+      default: "default",
       validator: function (value) {
-        return ['default', 'black', 'white'].indexOf(value) !== -1
-      }
+        return ["default", "black", "white"].indexOf(value) !== -1;
+      },
     },
     disabled: {
       type: Boolean,
       required: false,
-      default: false
-    }
+      default: false,
+    },
   },
-  head () {
-    const skip = googlePayScriptLoaded
-    googlePayScriptLoaded = true
+  head() {
+    const skip = googlePayScriptLoaded;
+    googlePayScriptLoaded = true;
 
     return {
       script: [
         {
-          vmid: 'googlepay',
+          vmid: "googlepay",
           once: true,
           skip,
-          src: 'https://pay.google.com/gp/p/js/pay.js',
-          callback: () => window.onGooglePayLoaded && window.onGooglePayLoaded()
-        }
-      ]
-    }
+          src: "https://pay.google.com/gp/p/js/pay.js",
+          callback: () =>
+            window.onGooglePayLoaded && window.onGooglePayLoaded(),
+        },
+      ],
+    };
   },
-  setup (props, { emit }) {
+  setup(props, { emit }) {
     const { $googlepay } = useVSFContext();
     const {
       merchantId,
@@ -72,69 +73,68 @@ export default {
       allowedCardNetworks,
       allowedAuthMethods,
       currencyCode,
-      environment
+      environment,
     } = $googlepay.config;
 
     const baseCardPaymentMethod = {
-      type: 'CARD',
+      type: "CARD",
       parameters: {
         allowedAuthMethods,
-        allowedCardNetworks
-      }
-    }
-
-    const tokenizationSpecification = {
-      type: 'PAYMENT_GATEWAY',
-      parameters: {
-        gateway,
-        gatewayMerchantId
-      }
+        allowedCardNetworks,
+      },
     };
 
-    const cardPaymentMethod = Object.assign(
-      {},
-      baseCardPaymentMethod,
-      {
-        tokenizationSpecification
-      }
-    )
+    const tokenizationSpecification = {
+      type: "PAYMENT_GATEWAY",
+      parameters: {
+        gateway,
+        gatewayMerchantId,
+      },
+    };
+
+    const cardPaymentMethod = Object.assign({}, baseCardPaymentMethod, {
+      tokenizationSpecification,
+    });
 
     const lineItems = computed(() => {
-      const lineItems = []
+      const lineItems = [];
 
       for (const key in props.totals) {
         if (Object.hasOwnProperty.call(props.totals, key)) {
           const amount = props.totals[key];
 
-          if (key === 'shipping') {
+          if (key === "shipping") {
             lineItems.push({
-              label: 'Shipping cost',
-              type: 'LINE_ITEM',
-              price: amount.toFixed(2)
-            })
+              label: "Shipping cost",
+              type: "LINE_ITEM",
+              price: amount.toFixed(2),
+            });
           } else if (lineItemsToDisplay.includes(key)) {
             lineItems.push({
               label: key,
               type: key.toUpperCase(),
-              price: amount.toFixed(2)
-            })
+              price: amount.toFixed(2),
+            });
           }
         }
       }
 
-      return lineItems
+      return lineItems;
     });
     const total = computed(() => props.totals?.total || 0);
     const buttonRef = ref();
 
     const getGooglePaymentsClient = () => {
-      if (paymentsClient === null && window?.google?.payments?.api?.PaymentsClient) {
+      if (
+        paymentsClient === null &&
+        window?.google?.payments?.api?.PaymentsClient
+      ) {
         paymentsClient = new window.google.payments.api.PaymentsClient({
-          environment: environment === ENV_PROD ? 'PRODUCTION' : 'TEST',
+          environment: environment === ENV_PROD ? "PRODUCTION" : "TEST",
           merchantInfo: {
             merchantName: merchantName,
-            merchantId: merchantId
-          }
+            merchantId: merchantId,
+          },
         });
       }
       return paymentsClient;
@@ -144,113 +144,119 @@ export default {
       return {
         displayItems: lineItems.value,
         currencyCode,
-        totalPriceStatus: 'FINAL',
+        totalPriceStatus: "FINAL",
         totalPrice: total.value.toString(),
-        totalPriceLabel: 'Total'
-      }
+        totalPriceLabel: "Total",
+      };
     };
 
     const getGooglePaymentDataRequest = () => {
-      const paymentDataRequest = Object.assign({}, baseRequest)
-      paymentDataRequest.allowedPaymentMethods = [cardPaymentMethod]
-      paymentDataRequest.transactionInfo = getGoogleTransactionInfo()
+      const paymentDataRequest = Object.assign({}, baseRequest);
+      paymentDataRequest.allowedPaymentMethods = [cardPaymentMethod];
+      paymentDataRequest.transactionInfo = getGoogleTransactionInfo();
       paymentDataRequest.merchantInfo = {
-        merchantName: merchantName
-      }
+        merchantName: merchantName,
+      };
 
       if (environment === ENV_PROD) {
-        paymentDataRequest.merchantInfo.merchantId = merchantId
+        paymentDataRequest.merchantInfo.merchantId = merchantId;
       }
 
-      return paymentDataRequest
+      return paymentDataRequest;
     };
 
     const processPayment = (paymentData) => {
-      emit('success', paymentData.paymentMethodData)
+      emit("success", paymentData.paymentMethodData);
     };
 
     const onGooglePaymentButtonClicked = async () => {
       if (props.disabled) {
-        return
+        return;
       }
-      emit('click');
-      const paymentDataRequest = getGooglePaymentDataRequest()
-      paymentDataRequest.transactionInfo = getGoogleTransactionInfo()
+      emit("click");
+      const paymentDataRequest = getGooglePaymentDataRequest();
+      paymentDataRequest.transactionInfo = getGoogleTransactionInfo();
 
       try {
-        const paymentsClient = getGooglePaymentsClient()
-        const paymentData = await paymentsClient.loadPaymentData(paymentDataRequest)
-        processPayment(paymentData)
+        const paymentsClient = getGooglePaymentsClient();
+        const paymentData = await paymentsClient.loadPaymentData(
+          paymentDataRequest
+        );
+        processPayment(paymentData);
       } catch (err) {
-        console.error(err)
+        console.error(err);
       }
     };
 
     const addGooglePayButton = () => {
-      const paymentsClient = getGooglePaymentsClient()
+      const paymentsClient = getGooglePaymentsClient();
       const button = paymentsClient.createButton({
         onClick: onGooglePaymentButtonClicked,
         buttonColor: props.color,
-        buttonType: props.type
-      })
-      buttonRef.value.appendChild(button)
+        buttonType: props.type,
+      });
+      buttonRef.value.appendChild(button);
     };
 
     const getGoogleIsReadyToPayRequest = () => {
-      return Object.assign(
-        {},
-        baseRequest,
-        {
-          allowedPaymentMethods: [baseCardPaymentMethod]
-        }
-      )
+      return Object.assign({}, baseRequest, {
+        allowedPaymentMethods: [baseCardPaymentMethod],
+      });
     };
 
     const prefetchGooglePaymentData = () => {
-      const paymentDataRequest = getGooglePaymentDataRequest()
+      const paymentDataRequest = getGooglePaymentDataRequest();
       paymentDataRequest.transactionInfo = {
-        totalPriceStatus: 'NOT_CURRENTLY_KNOWN',
-        currencyCode
-      }
-      const paymentsClient = getGooglePaymentsClient()
-      paymentsClient.prefetchPaymentData(paymentDataRequest)
+        totalPriceStatus: "NOT_CURRENTLY_KNOWN",
+        currencyCode,
+      };
+      const paymentsClient = getGooglePaymentsClient();
+      paymentsClient.prefetchPaymentData(paymentDataRequest);
     };
 
     const setupButton = async () => {
-      const paymentsClient = getGooglePaymentsClient()
-      const isReadyToPayRequest = getGoogleIsReadyToPayRequest()
+      const paymentsClient = getGooglePaymentsClient();
+      const isReadyToPayRequest = getGoogleIsReadyToPayRequest();
 
       try {
-        const response = await paymentsClient.isReadyToPay(isReadyToPayRequest)
+        const response = await paymentsClient.isReadyToPay(isReadyToPayRequest);
         if (response.result) {
-          addGooglePayButton()
-          prefetchGooglePaymentData()
+          addGooglePayButton();
+          prefetchGooglePaymentData();
         }
       } catch (err) {
-        console.error(err)
+        console.error(err);
       }
     };
 
     onMounted(() => {
-      const paymentsClient = getGooglePaymentsClient()
+      const paymentsClient = getGooglePaymentsClient();
       if (paymentsClient) {
         setupButton();
       } else {
         window.onGooglePayLoaded = () => {
           setupButton();
           window.onGooglePayLoaded = null;
-        }
+        };
       }
     });
 
     return {
       buttonRef,
-    }
-  }
-}
+    };
+  },
+};
 </script>
 
 <style>
+.google-pay-button {
+  display: flex;
+  overflow: hidden;
+}
+.google-pay-button > div {
+  display: flex;
+  max-width: 100%;
+}
 .google-pay-button button {
   display: block;
 }
